e07095c03405b9018ed89385653acce9865f70da
Rajeev's art gallery <rajeevansh.sikkim@gmail.com>
2020-11-07 17:49:55 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('js--scroll-to-plans').click(function(){
+     $('html,body').animate(scrollTop:$('js--section-plans').offset().top),1000);
+    
+     } );
+});
